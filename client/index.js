0808737be05ae54085ee4e1ec43c79191cef65b2
BuildTools <unconfigured@null.spigotmc.org>
2021-04-05 13:19:09 -0500

    test

--- client/index.js
+++ client/index.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function loadHTMLTable(data) {
     const table = document.querySelector('table tbody');
-
     if (data.length == 0) {
         table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</tr>"
     }
