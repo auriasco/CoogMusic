745ceb9c314d06c0ca987f0bbb9d2e6e7a0ba2cd
BuildTools <unconfigured@null.spigotmc.org>
2021-04-05 14:18:00 -0500

    integrating

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
 const express = require('express');
-const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const path = require('path');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
-const dbService = require('./dbService');
+//start server
+const app  = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended : false}));
-
-// create
-app.post('/insert', (request, response) => {
-    console.log(request.body);
+dotenv.config({
+    path: './.env'
 });
 
-// read
-app.get('/getAll', (request, response) => {
-    const db = dbService.getDbServiceInstance();
-    const result = db.getAllData();
-    result
-    .then(data => response.json({data: data}))
-    .catch(err => console.log(err));
-})
+//database
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DATABASE_PORT 
+});
 
-// update
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
 
-// delete
+//Parse URL-encoded bodies (As sen tby HTML forms)
 
-app.listen(process.env.PORT, () => console.log('app is running'));
+app.use(express.urlencoded({extended: false}));
+
+//values we get from form = JSON
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'hbs'); //template HTML
+
+db.connect((er) =>{
+    if(er){
+        console.log(er);
+    }else{
+        console.log('DB connected');
+    }
+});
+
+app.use(cookieParser('keyboard cat'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+
+  
+
+//Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(5000, ()=>{
+    console.log("Server started");
+});
