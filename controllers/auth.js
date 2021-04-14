55ac5b243b5ddfe895fc5322d8bd4f35fc10dc75
notgaby <45638369+notgaby@users.noreply.github.com>
2021-04-14 18:21:19 -0500

    Update auth.js
    Added edit routes, added cookies middleware and creation of cookies ***

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const mysql = require('sync-mysql');
 const mysqladd = require('mysql2');
 const { v4: uuidv4 } = require('uuid');
 const e = require('express');
+const { async } = require('q');
+const { promisify } = require ('util');
 //const flash = require('connect-flash');
 
 
@@ -24,49 +26,69 @@ const db2 = mysqladd.createConnection({
 });
 
 
-/*
-async function userExistsCheck(tableUserVar, tableName, testVal){
-    try{
-        db.promise().query(`SELECT ${tableUserVar} FROM ${tableName} WHERE ${tableUserVar} = ?`, [testVal],(err, results)=>{
-            if(err){
-                console.log('i wanna die');
-                console.log(err);
-                return false;
+exports.logout = async (req, res)=>{
+    res.cookie('account', 'logout',{
+        expires: new Date(Date.now() + 2*1000),
+        httpOnly:  true
+    });
+    res.redirect('/');
+
+}
+
+//middleman
+exports.getAccount = async (req, res, next) =>{
+    if(req.cookies.account){
+        try{
+            //vverify token
+            const decoded  =  await promisify(jwt.verify)(req.cookies.account, process.env.TOKEN_SECRET);
+            
+            if(decoded.type == 'User'){
+                //check user
+                let results = db.query(`SELECT * FROM User WHERE user_id = ?`, [decoded.id]);
+                if(results <= 0){
+                    console.log("DNE");
+                    return next(); //DNE
+                }
+                req.acc = results[0];
+                //console.log("HSAHDHSAHD: " + results[0].user_name_display);
+                return next();
+
+            }else if(decoded.type == 'Artist'){
+                //check musician
+                console.log("YERRR");
+
+                //check user
+                let results = db.query(`SELECT * FROM Artist WHERE artist_id = ?`, [decoded.id]);
+                if(results <= 0){
+                    console.log("DNE");
+                    return next(); //DNE
+                }
+                console.log("EEEE");
+                req.acc = results[0];
+                //console.log("HSAHDHSAHD: " + results[0].user_name_display);
+                return next();
             }
 
-            if(results.length > 0){
-                console.log('exists');
-                return true;
-            }
-        })
-        
-    }catch(err){
-        console.log(err);
-        return false;
+
+        }catch(error){
+            console.log(error);
+            return next();
+        }
+    }else{
+        next();
     }
 }
-*/
-
-exports.viewUsers = (req, res)=>{
-
-    let users = db.query(`SELECT * FROM User`);
-    //console.log(users);
-    req.flash('data', users);
-    return res.redirect('/viewUsers');
-
-}
-
 
 exports.register =  (req, res)=>{
     console.log(req.body);
-
-    const { name, username, email, DOB, country, password, password2} = req.body;
+    const { name, username, email, DOB, country, password, password2, biography, fb_url, ig_url, spotify_url, soundcloud_url, personal_url} = req.body;
     let isMusician = req.body.checkbox ? true : false
 
+   
     let results = db.query(`SELECT user_name FROM User WHERE user_name = ?`, [username]);
     if(results.length > 0){
         return res.render('register', {
-            message: 'Try again: That username is already in use'
+            message: 'Try again: That username is already in use by another user'
         });
     }
 
@@ -89,34 +111,165 @@ exports.register =  (req, res)=>{
 
     const artist_id = 0; //SET TO 0 RN CHANGE LATER
 
-    //Not a musician 
+    //Not a musician, so create a user account
     if(isMusician === false)
     {
         db2.query(`INSERT INTO User SET ?`, {artist_idF: artist_id, user_id: uniqueId, user_name: username, user_email: email, country: country, age: currAge , user_password: password, user_name_display: name});
-        req.flash('welcomeName', username);
-        req.flash('userId', uniqueId);
-        req.flash('userName', name);
-        req.flash('Birth',DOB);
-        req.flash('Country',country);
-        req.flash('email',email);
-        return res.redirect('/successRegister_User');
-        //return res.render('register', {
-        //    message: 'User registered' 
-        // });
+        const token = jwt.sign({id: uniqueId, type: 'User'}, process.env.TOKEN_SECRET, {expiresIn: process.env.TOKEN_EXPIRES_IN} );
 
+        const cookieOptions = {
+            expires: new Date(
+                Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+            ),
+            httpOnly: true
+        }
+        
+        res.cookie('account', token, cookieOptions);
+        return res.redirect('/successRegister_User');
 
     }else{
-        db2.query(`INSERT INTO Artist SET ?`, {artist_id: uniqueId, artist_name: username, artist_email: email, country: country, background_link: 'null', website_url: 'null', artist_password: password, artist_name_display: name});
-        req.flash('welcomeName', username);
-        req.flash('userId', uniqueId);
-        req.flash('userName', name);
-        req.flash('Birth',DOB);
-        req.flash('Country',country);
-        req.flash('email',email);
+        
+        db2.query(`INSERT INTO Artist SET ?`, {artist_id: uniqueId, artist_name: username, artist_email: email, country: country, background_link: '', website_url: personal_url, artist_password: password, artist_name_display: name, biography: biography, fb_url: fb_url, ig_url: ig_url, spotify_url: spotify_url, soundcloud_url: soundcloud_url, age: currAge });
+        const token = jwt.sign({id: uniqueId, type: 'Artist'}, process.env.TOKEN_SECRET, {expiresIn: process.env.TOKEN_EXPIRES_IN} );
+
+        const cookieOptions = {
+            expires: new Date(
+                Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+            ),
+            httpOnly: true
+        }
+        
+        res.cookie('account', token, cookieOptions);
+        
         return res.redirect('/successRegister_Artist');
     }   
         
 }
+
+exports.updateArtistProfile = (req, res)=>{
+    console.log(req.body);
+
+    const {name, email, DOB, country, password, password2, biography, fb_url, ig_url, spotify_url, soundcloud_url, personal_url, artistId} = req.body;
+    const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10)
+    const currAge = getAge(req.body.DOB);
+
+
+    db2.query(`UPDATE Artist SET ? WHERE ?`, [{country: country}, {artist_id: artistId}], (err)=>{
+        if(err){
+            console.log('else');
+            console.log(err);
+        }
+    });
+
+    if(name != ''){
+
+        db2.query(`UPDATE Artist SET ? WHERE ?`, [{artist_name_display: name}, {artist_id: artistId}], (err)=>{
+            if(err){
+                console.log('name');
+                console.log(err);
+            }
+            
+
+        });
+    }
+    
+    if(email != ''){
+        console.log("UPDATE EMAIL");
+
+        db2.query(`UPDATE Artist SET ? WHERE ?`, [{artist_email: email}, {artist_id: artistId}], (err)=>{
+            if(err){
+                console.log('email');
+                console.log(err);
+            }
+            
+        });
+
+        console.log("new e: " + email);
+    }
+
+    if(password != ''){
+
+        if(password != password2){
+            console.log("flash s tart");
+            req.flash('error', 'Try again: Passwords do not match');
+            console.log("flash end");
+            return res.redirect('/editArtistProfile');
+        }
+
+        db2.query(`UPDATE Artist SET ? WHERE ?`, [{artist_password: password}, {artist_id: artistId}], (err)=>{
+            if(err){
+                console.log('pass');
+                console.log(err);
+            }
+        });
+        
+    }
+
+    if(biography != ''){
+        db2.query(`UPDATE Artist SET ? WHERE ?`, [{biography: biography}, {artist_id: artistId}], (err)=>{
+            if(err){
+                console.log('pass');
+                console.log(err);
+            }
+        });
+
+    }
+
+    if(fb_url != ''){
+        db2.query(`UPDATE Artist SET ? WHERE ?`, [{fb_url: fb_url}, {artist_id: artistId}], (err)=>{
+            if(err){
+                console.log('pass');
+                console.log(err);
+            }
+        });
+
+    }
+
+    if(ig_url != ''){
+        db2.query(`UPDATE Artist SET ? WHERE ?`, [{ig_url: ig_url}, {artist_id: artistId}], (err)=>{
+            if(err){
+                console.log('pass');
+                console.log(err);
+            }
+        });
+
+    }
+
+    if(spotify_url != ''){
+        db2.query(`UPDATE Artist SET ? WHERE ?`, [{spotify_url: spotify_url}, {artist_id: artistId}], (err)=>{
+            if(err){
+                console.log('pass');
+                console.log(err);
+            }
+        });
+
+    }
+
+    if(soundcloud_url != ''){
+        db2.query(`UPDATE Artist SET ? WHERE ?`, [{soundcloud_url: soundcloud_url}, {artist_id: artistId}], (err)=>{
+            if(err){
+                console.log('pass');
+                console.log(err);
+            }
+        });
+
+    }
+
+    if(personal_url != ''){
+        db2.query(`UPDATE Artist SET ? WHERE ?`, [{website_url: personal_url}, {artist_id: artistId}], (err)=>{
+            if(err){
+                console.log('pass');
+                console.log(err);
+            }
+        });
+
+    }
+
+    
+    req.flash('success', 'Successfully updated information');
+    return res.redirect('/editArtistProfile');
+
+};
 
 exports.updateUserProfile = (req, res)=>{
     console.log(req.body);
@@ -133,6 +286,8 @@ exports.updateUserProfile = (req, res)=>{
         }
     });
 
+
+
     if(name != ''){
 
         db2.query(`UPDATE User SET ? WHERE ?`, [{user_name_display: name}, {user_id: userId}], (err)=>{
@@ -143,13 +298,11 @@ exports.updateUserProfile = (req, res)=>{
             
 
         });
-        req.flash('displayName', name);
 
     }else{
         console.log('name not changing');
         let nameOld = db.query(`SELECT user_name_display FROM User WHERE user_id = ?`, [userId]);
         console.log(nameOld);
-        req.flash('displayName', nameOld.user_name_display);
     }
     
     if(email != ''){
@@ -161,15 +314,20 @@ exports.updateUserProfile = (req, res)=>{
             }
             
         });
-        req.flash('email',email);
     }else{
         //req.flash('displayName', name);
         let emailOld = db.query(`SELECT user_email FROM User WHERE user_id = ?`, [userId]);
-        req.flash('email', emailOld.user_email);
 
     }
 
     if(password != ''){
+
+        if(password != password2){
+            console.log("flash s tart");
+            req.flash('error', 'Try again: Passwords do not match');
+            console.log("flash end");
+            return res.redirect('/editProfile');
+        }
 
         db2.query(`UPDATE User SET ? WHERE ?`, [{user_password: password}, {user_id: userId}], (err)=>{
             if(err){
@@ -180,47 +338,9 @@ exports.updateUserProfile = (req, res)=>{
         
     }
 
-    /*
-    req.flash('welcomeName', username);
-    req.flash('userId', uniqueId);
-    req.flash('userName', name);
-    req.flash('Birth',DOB);
-    req.flash('Country',country);
-    req.flash('email',email);
-    */
-    req.flash('userId', userId);
-    req.flash('DOB',DOB);
-    req.flash('country',country);
-    console.log('end');
-
-    return res.redirect('/editProfile');
-
-
     
-
-
-    /*
-    const data = req.body;
-    let updateVals = {};
-    Object.keys(data).forEach((prop)=>{
-        if(req.body[prop] != ''){
-            if(prop == '')
-            updateVals[prop] = req.body[prop];
-        }
-    });
-
-    updateVals['age'] = currAge;
-
-    console.log(updateVals);
-
-
-    db2.query(`UPDATE User SET ? WHERE user_id = ?`, [{},{user_id: updateVals.userId}])
-    */
-
-    //db2.query(
-        //`UPDATE User SET user_email = COALESCE(${email}, user_email), country = COALESCE(${country}, country), age = COALESCE(${currAge}, age), user_password = COALESCE(${password}, user_password), user_name_display = COALESCE(${name}, user_name_display) WHERE user_id = ${userId} AND (${email} IS NOT NULL AND ${email} IS DISTINCT FROM user_email) OR (${country} IS NOT NULL AND ${country} IS DISTINCT FROM country) OR (${currAge} IS NOT NULL AND ${currAge} IS DISTINCT FROM age) OR (${password} IS NOT NULL AND ${password} IS DISTINCT FROM user_password) OR (${name} IS NOT NULL AND ${name} IS DISTINCT FROM user_name_display));` 
-    //    `UPDATE User SET user_email = ${email}, country = ${country}, age = ${currAge}, user_password = ${password}, user_name_display = ${name} WHERE user_id = ${userId} AND (${email} IS NOT NULL) OR (${country} IS NOT NULL) OR (${currAge} IS NOT NULL) OR (${password} IS NOT NULL) OR (${name} IS NOT NULL);` 
-    //    );
+    req.flash('success', 'Successfully updated information');
+    return res.redirect('/editProfile');
 
 };
 
@@ -246,31 +366,35 @@ exports.login  = (req, res) =>{
     }else{
 
         if(results.length <= 0){
-            //artist
-            req.flash('welcomeName', username);
-            req.flash('userId', results2[0].user_id);
-            req.flash('userName', results2[0].user_name_display);
-            //req.flash('Birth',results[0].);
-            req.flash('Country',results2[0].country);
-            req.flash('email',results2[0].user_email);
-            return res.redirect('/artist_index');
+
+            //results is empty so results2 has  user = we are  looking at ARTISTS
+
+            const token = jwt.sign({id: results2[0].artist_id, type: 'Artist'}, process.env.TOKEN_SECRET, {expiresIn: process.env.TOKEN_EXPIRES_IN} );
+
+                const cookieOptions = {
+                    expires: new Date(
+                        Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+                    ),
+                    httpOnly: true
+                }
+                
+                res.cookie('account', token, cookieOptions);
+                return res.redirect('/artist_index');
 
         }else{
             //user
-            //login successful
-            req.flash('welcomeName', username);
-            req.flash('userId', results[0].user_id);
-            req.flash('userName', results[0].user_name_display);
-            //req.flash('Birth',results[0].);
-            req.flash('Country',results[0].country);
-            req.flash('email',results[0].user_email);
-            return res.redirect('/user_index');
+            const token = jwt.sign({id: results[0].user_id, type: 'User'}, process.env.TOKEN_SECRET, {expiresIn: process.env.TOKEN_EXPIRES_IN} );
 
+            const cookieOptions = {
+                expires: new Date(
+                    Date.now() + process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+                ),
+                httpOnly: true
+            }
+            
+            res.cookie('account', token, cookieOptions);
+            return res.redirect('/user_index');
         }
 
     }
-
-
-
-    
 };
