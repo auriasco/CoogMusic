b5c26efd64496c50195e62759c8179c48f85b4ff
notgaby <45638369+notgaby@users.noreply.github.com>
2021-04-14 18:20:00 -0500

    Update auth.js
    Create logout route and admin routes and update profile routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const authController = require('../controllers/auth');
-
+const adminController = require('../controllers/adminActions');
 const router = express.Router();
 
 //auth/register auth coming from app.js
 router.post('/register', authController.register);
 router.post('/updateUserProfile',  authController.updateUserProfile);
+router.post('/updateArtistProfile',  authController.updateArtistProfile);
 router.post('/login',  authController.login);
-router.get('/viewUsers',  authController.viewUsers);
+router.get('/logout', authController.logout);
+
+
+//admin
+router.get('/viewUsers',  adminController.viewUsers);
+router.get('/viewArtistsAdmin', adminController.viewArtists);
+
+
 
 module.exports = router;
