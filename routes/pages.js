70124406f47bc1a9a910b9f261f4506cb7ee5ef8
notgaby <45638369+notgaby@users.noreply.github.com>
2021-04-15 11:41:33 -0500

    Update pages.js
    added notes

--- routes/pages.js
+++ routes/pages.js
@@ -17,6 +17,60 @@ router.get('/login', (req,res)=>{
     res.render('login');
 });
 
+
+/*
+USE OF COOKIES: authController.getAccount = the middleware function to  get the cookies
+the getAccount exported function in the auth.js file in the CONTROLLERS folder.
+
+how it works: if user/artist clicks on the edit profile button for example,
+will first go to the middleware function that gets the cookies from the browser.
+(whenever you login or register, a cookie will be registered automatically)
+This cookie is called req.acc
+req.acc will contain the identifying info of the user or artist's id and ttheir type (user or artist)
+Useful because since we now have their id, we can access their stuff in the database/
+req.acc will give us everything in the arttist or user table associated with that id.
+Ex- req.acc (we are looking at artists) will give us all their info from the table
+such as their display name, username, social media links, etc.
+
+*/
+
+//////////////// ARTIST PAGE ////////////////
+router.get('/editArtistProfile', authController.getAccount, (req, res)=>{
+    if(req.acc){
+        //get the cookie info and store it in a variable named acc.
+        //Then in the hbs files, we can get the info using {{acc.artist_name}}, etc.
+        //error and success are flashes sent by the edit artist profile function in the auth.js file in
+        //the CONTROLLERS folder. Error = passwords dont match, etc. 
+        //It gets put in here as well because when you press submit (to  edit your info),
+        //it goes to /auth/editArtistProfile => redirects back to /editArtistProfile when
+        //finished updating or it catches an error
+
+        res.render('editArtistProfile', {acc: req.acc, error: req.flash('error'), success: req.flash('success')});
+    }else{
+        res.redirect('/login');
+    }
+});
+
+router.get('/successRegister_Artist', authController.getAccount, (req, res)=>{
+    if(req.acc){
+        res.render('successRegister_Artist', {acc: req.acc});
+    }else{
+        res.redirect('/login');
+    }
+});
+
+
+router.get('/artist_index', authController.getAccount, (req, res)=>{
+    if(req.acc){
+        res.render('artist_index', {acc: req.acc});
+    }else{
+        res.redirect('/login');
+    }
+});
+
+////////////////////////////////////////////////
+
+
 //////////////// USER PAGE ////////////////////////////////
 
 router.get('/successRegister_User', authController.getAccount, (req, res)=>{
@@ -26,6 +80,7 @@ router.get('/successRegister_User', authController.getAccount, (req, res)=>{
     }else{
         res.redirect('/login');
     }
+
 });
 
 router.get('/user_index', authController.getAccount, (req, res)=>{
@@ -57,46 +112,10 @@ router.get('/viewArtistsAdmin', (req, res)=>{
 });
 
 router.get('/admin_index', (req, res)=>{
-    console.log('GET');
     res.render('admin_index');
-    //req.flash('welcomeName',messages);
-    //res.render('user_index',{messages: req.flash('welcomeName')});
 });
 
 ////////////////////////////////////////////////
-
-
-
-//////////////// ARTIST PAGE ////////////////
-router.get('/editArtistProfile', authController.getAccount, (req, res)=>{
-    if(req.acc){
-        res.render('editArtistProfile', {acc: req.acc, error: req.flash('error'), success: req.flash('success')});
-    }else{
-        res.redirect('/login');
-    }
-});
-
-router.get('/successRegister_Artist', authController.getAccount, (req, res)=>{
-    if(req.acc){
-        res.render('successRegister_Artist', {acc: req.acc});
-    }else{
-        res.redirect('/login');
-    }
-});
-
-
-router.get('/artist_index', authController.getAccount, (req, res)=>{
-    if(req.acc){
-        res.render('artist_index', {acc: req.acc});
-    }else{
-        res.redirect('/login');
-    }
-});
-
-
-
-////////////////////////////////////////////////
-
 
 
 
