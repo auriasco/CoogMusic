f5d8cc332aeb90ab131f9e50ae0f7f840766304d
Salar Salar <ssalar8998@gmail.com>
2021-04-14 20:27:51 -0500

    added fileUpload method and changed uploadMusic routes in views

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -16,10 +16,55 @@ const db = new mysql({
 });
 
 
-exports.index = function(req, res){
+exports.Upload = function(req, res){
     message = '';
     if(req.method == "POST"){
-      var post  = req.body;
-   }
- 
+        var post  = req.body;
+        var song_Name= post.songName;
+        var artist_Name= post.artistName;
+        var album_Name= post.albumName;
+        var release_Date= post.releaseDate;
+
+    if(req.method == "POST"){
+        if (!req.files)
+            return res.status(400).send('No files were uploaded.');
+    
+        var file_Img = req.files.uploaded_image;
+        var img_name = file_Img.name;
+        var file_Audio = post.fileAudio;
+        var audio_name = file_Audio.name;
+
+        if(file_Img.mimetype == "image/jpeg" ||file_Img.mimetype == "image/png"||file_Img.mimetype == "image/gif" ){
+                                    
+            file_Img.mv('public/song_images/'+file_Img.name, function(err) {
+                                
+                if (err)
+                    return res.status(500).send(err);
+                var sql = "INSERT INTO `Song`(`song_name`,`artist_name`,`album_name`,`release_date`, `song_img_path`) VALUES ('" + song_Name + "','" + artist_Name + "','" + album_Name + "','" + release_Date + "','" + img_name + "')";
+    
+                var query = db.query(sql, function(err, result) {
+                res.redirect('profile/'+result.insertId);
+                    });
+            });
+
+            file_Audio.mv('public/song_audio/'+file_Audio.name, function(err){
+                if(err)
+                    return res.status(500).send(err);
+                
+                var sql = "INSERT INTO `Song`(`song_mp3_path`"
+            });
+
+
+        } 
+        else{
+            message = "This format is not allowed , please upload file with '.png','.gif','.jpg'";
+            res.render('index.ejs',{message: message});
+        }
+    }   
+    else{
+        res.render('index');
+    }
+    
+    }
+
 };
